3bc7cb3985b0d9eb0cef548e0c9cf84fd2118567
DarkCode <josemanuelramirezrengifo9@gmail.com>
2025-10-01 11:05:38 -0400

    Update game_calculator.js

--- game_calculator.js
+++ game_calculator.js
@@ -1,5 +1,28 @@
-const usr_module = require('readline');
+function game(){
+  const readlineSync = require('readline-sync');
+  const messageWarning = "¡Advertencia! si usted no usa o tiene instalado 'CommonJs', no sé podrá continuar la ejecución del juego";
 
-console.log("¡Hola, usuario!");
+  console.log(`${messageWarning}`);
+  console.log("");
 
-// Resolviendo el problema con el input....
+  const userName = readlineSync.question("¿Cómo te llamas?: ");
+  console.log("")
+
+  console.log(`¡Qué bonito nombre ${userName}!`);
+
+  const questionForTheUser = readlineSync.question(`${userName}, ¿Quiéres jugar?: `);
+
+  if (questionForTheUser === "si" || questionForTheUser === "sí"){
+    console.log("Comenzando el juego....");
+  }
+}
+  else if (question === "no"){
+    console.log("Saliendo del juego....");
+  }
+
+console.log("Gracias por jugar :)");
+console.log("");
+
+console.log(`¡Cuídate ${userName}! ^_^`);
+
+game();
