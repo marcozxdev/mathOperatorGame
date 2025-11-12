fe3bbd4aa5a91f2dd6e422d4dc5c3ab478c95bdd
DarkCode <josemanuelramirezrengifo9@gmail.com>
2025-11-12 19:18:26 -0400

    Implement simple math game in main.cpp

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+void game() {
+  string name;
+  int answer = 0;
+  
+  cout << "¿Cómo te llamas?: ";
+  cin >> name;
+  cout << "\n";
+
+  cout << "Mucho gusto " + name << endl;
+
+  while (true) {
+    cout << "¿Cuánto es 60 + 80?: "
+    cin >> answer;
+    cout << "\n";
+
+    if (answer == 140) {
+      cout << "¡Es correcto!" << endl;
+      break;
+    }
+    else {
+      cout << "¡Es incorrecto!" << endl;
+      continue;
+    }
+  }
+}
+
+int main() {
+  cout << "Hola usuario" << endl;
+  game();
+  
+  return 0;
+}
