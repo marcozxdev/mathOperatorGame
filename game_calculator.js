4db2388a066e45b14fe949d40b80fc5610a6af39
DarkCode <josemanuelramirezrengifo9@gmail.com>
2025-11-12 21:57:10 -0400

    Refactor game function and improve user prompts

--- game_calculator.js
+++ game_calculator.js
@@ -1,4 +1,4 @@
-function game(){
+const game = () {
   const readlineSync = require('readline-sync');
   const messageWarning = "¡Advertencia! si usted no usa o tiene instalado 'CommonJs', no sé podrá continuar la ejecución del juego";
 
@@ -6,7 +6,7 @@ function game(){
   console.log("");
 
   const userName = readlineSync.question("¿Cómo te llamas?: ");
-  console.log("")
+  console.log("");
 
   console.log(`¡Qué bonito nombre ${userName}!`);
 
@@ -14,15 +14,34 @@ function game(){
 
   if (questionForTheUser === "si" || questionForTheUser === "sí"){
     console.log("Comenzando el juego....");
+
+    const number = undefined;
+    let trys = 0;
+
+    while (true) {
+      let userPlaying = readlineSync.question(`${userName} por favor adivina un número del 1 al 100: `);
+      userPlaying = parseInt(userPlaying);
+      trys += 1;
+
+      if (userPlaying < number) {
+        console.log("Demasiado bajo");
+      }
+      else if (userPlaying > number) {
+        console.log("Demasiado alto");
+      }
+      else if (userPlaying === number) {
+        console.log(`¡Felicidades ${userName} has encontrado el número ${number} en ${trys} intentos`);
+        break;
+      }
+    }
   }
-}
   else if (question === "no"){
     console.log("Saliendo del juego....");
   }
 
-console.log("Gracias por jugar :)");
-console.log("");
+  console.log("Gracias por jugar :)");
+  console.log("");
 
-console.log(`¡Cuídate ${userName}! ^_^`);
-
+  console.log(`¡Cuídate ${userName}! ^_^`);
+}
 game();
