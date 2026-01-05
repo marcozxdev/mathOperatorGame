f1ddc9449b642b040b1a2dcfa935c29df5dfb3d1
DarkCode <josemanuelramirezrengifo9@gmail.com>
2026-01-05 17:36:05 -0400

    Refactor game function and simplify user prompts

--- game_calculator.py
+++ game_calculator.py
@@ -2,48 +2,20 @@ import sys
 import time
 import random
 
-level = """1. Fácil 2. Medio 3. Avanzado
-
-¡Diviértete! :)
-
-"""
-
 def game():
-    try:
-        user_name = input("¿Cómo te llamas?: ").title()
-        print(f"{level}\n")
+    print("------ Iniciando juego ------")
 
-        question = input(f"¿{user_name} cuál elijes?: ").lower()
-        question.strip()
+    mini_games = """
+    ---------------
+    #             #
+    #             #
+    #             #
+    ---------------
+    """
 
-        if question == "basico" or question == "básico":
-            print("Iniciando el juego....")
-            time.sleep(1)
-            select_operation = input(user_name + " \n¿con que operación quieres jugar. Suma, resta, multi o div?: ").lower()
-            select_operation.strip()
-            
-            if select_operation == "suma" or select_operation == "sumar":
-                pass
-        
-        elif question == "medio":
-            print("Iniciando el juego....")
-            time.sleep(1)
-            select_operation = input(user_name + " \n¿con que operación quieres jugar. Suma, resta, multi o div?: ").lower()
-            select_operation.strip()
-            
-            if select_operation == "suma" or select_operation == "sumar":
-                pass
-        
-        elif question == "avanzado":
-            print("Iniciando el juego....")
-            time.sleep(1)
-            select_operation = input(user_name + " \n¿con que operación quieres jugar. Suma, resta, multi o div?: ").lower()
+    # print(mini_games)
 
-            if select_operation == "suma" or select_operation == "sumar":
-                pass
-    
-    except EOFError:
-        print("No estás en un entorno interactivo, tu entorno no pemrite entrada (input)")
+    select_mini_game = input("Seleccione un juego para jugar": )
 
-if __name__ == '__main__':
-    game()
+    if select_mini_game in (None, None, None):
+        pass
