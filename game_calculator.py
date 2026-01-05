fa5079c614068091129ce295ce0a71099a8065a8
DarkCode <josemanuelramirezrengifo9@gmail.com>
2026-01-05 17:47:31 -0400

    Update mini games list and selection logic

--- game_calculator.py
+++ game_calculator.py
@@ -6,16 +6,24 @@ def game():
     print("------ Iniciando juego ------")
 
     mini_games = """
-    ---------------
-    #             #
-    #             #
-    #             #
-    ---------------
-    """
+      -------------------
+      # 1) Adivinanzas  #
+      # 2) Sumas        #
+      # 3) Adventuras   #
+      -------------------
+      """
 
-    # print(mini_games)
+    print(mini_games)
 
     select_mini_game = input("Seleccione un juego para jugar": )
+    select_mini_game = select_mini_game.strip().lower()
 
-    if select_mini_game in (None, None, None):
+    if select_mini_game == "adivinanzas":
         pass
+    elif select_mini_game == "sumas":
+        pass
+    elif select_mini_game == "adventuras"
+        pass
+
+if __name__ == '__main__':
+    game()
